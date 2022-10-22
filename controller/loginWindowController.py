3eb469ce53b990567e378da24434b4b0266c2dbb
Amr Aboughazala <amr.abughazala@gmail.come>
2022-10-22 15:48:29 +0200

    [added feature] adding emailManager.py

--- controller/loginWindowController.py
+++ controller/loginWindowController.py
@@ -1,12 +1,19 @@
+from view.viewFactory import ViewFactory
 from PyQt5 import QtWidgets, uic, QtGui, QtCore
+
+
 class LoginWindowController(QtWidgets.QWidget):
-    def __init__(self, viewFactory, uiPath, *args, **kwargs):
+    __emailManager: EmailManager
+    __viewFactory: ViewFactory
+    __uiPath: str
+    def __init__(self, emailManager, viewFactory, uiPath, *args, **kwargs):
         super(LoginWindowController, self).__init__(*args, **kwargs)
-        self.viewFactory = viewFactory
-        self.uiPath = uiPath
+        self.__emailManager = emailManager
+        self.__viewFactory = viewFactory
+        self.__uiPath = uiPath
 
     def initializeWindow(self):
-        uic.loadUi(self.uiPath, self)
+        uic.loadUi(self.__uiPath, self)
         self.bindToControls()
         self.show()
 
@@ -14,5 +21,5 @@ class LoginWindowController(QtWidgets.QWidget):
         self.login_btn.clicked.connect(self.on_login_btn)
 
     def on_login_btn(self):
-        self.viewFactory.mainWindow.initializeWindow()
+        self.__viewFactory.mainWindow.initializeWindow()
 
--- controller/mainWindowController.py
+++ controller/mainWindowController.py
@@ -1,10 +1,22 @@
-from PyQt5 import QtWidgets, uic, QtGui, QtCore
+from view.viewFactory import ViewFactory
+from PyQt5 import QtWidgets, uic, QtGui, QtCore, QtWebEngineWidgets
+
+
 class MainWindowController(QtWidgets.QMainWindow):
-    def __init__(self, viewFactory, uiPath, *args, **kwargs):
+    __emailManager: EmailManager
+    __viewFactory: ViewFactory
+    __uiPath: str
+    def __init__(self, emailManager, viewFactory, uiPath, *args, **kwargs):
         super(MainWindowController, self).__init__(*args, **kwargs)
-        self.viewFactory = viewFactory
-        self.uiPath = uiPath
+        self.__viewFactory = viewFactory
+        self.__uiPath = uiPath
 
     def initializeWindow(self):
-        uic.loadUi(self.uiPath, self)
+        uic.loadUi(self.__uiPath, self)
+        self.bindToControls()
         self.show()
+
+    def bindToControls(self):
+        url = "https://www.google.co.jp"
+        self.emailWebView.setUrl(QtCore.QUrl(url))
+
